749610efb8e8df51140e5bc3380fb415753b4ff0
CHAT-DISPARU <gaetanfranca@proton.me>
2026-06-10 03:05:35 +0200

    fix sphere inverse solution ...

--- srcs/Hittables/Plane.cpp
+++ srcs/Hittables/Plane.cpp
@@ -6,7 +6,7 @@
 /*   By: CHAT-DISPARU <CHAT-DISPARU@student.42.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/06 17:15:29 by gajanvie          #+#    #+#             */
-/*   Updated: 2026/06/10 01:26:15 by CHAT-DISPAR      ###   ########.fr       */
+/*   Updated: 2026/06/10 02:43:08 by CHAT-DISPAR      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,14 +22,14 @@ Plane::Plane(float point, const Mat4f &m, Material *mat)
 
 bool	Plane::hit(const Ray& ray, float t_min, float t_max, HitRecord& rec) const
 {
-	Ray		l_ray;
+	Ray		l_ray = ray;
 	Vec3f	local_p;
 	float	denom;
 	float	t;
 
 	l_ray *= _inverse;
 	denom = l_ray._dir._y;
-	if (fabs(denom) < t_min)
+	if (fabs(denom) < 1e-6f)
 		return (false);
 	t = -l_ray._o._y / denom;
 	if (t < t_min || t > t_max)
--- srcs/Hittables/Sphere.cpp
+++ srcs/Hittables/Sphere.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   Sphere.cpp                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gajanvie <gajanvie@student.42.fr>          +#+  +:+       +#+        */
+/*   By: CHAT-DISPARU <CHAT-DISPARU@student.42.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/06 13:14:43 by gajanvie          #+#    #+#             */
-/*   Updated: 2026/06/08 17:52:54 by gajanvie         ###   ########.fr       */
+/*   Updated: 2026/06/10 02:57:57 by CHAT-DISPAR      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,19 +48,19 @@ Sphere::Sphere(float d, Vec3f center, const Mat4f &m, Material *mat)
 	deter plus facile a calculer
 
 	−b±√b²-4ac
-    	2a
+		2a
 	−(−2g)±√4(g)²−4ac
-            2a
+			2a
 
 	2g±2√g²−ac  ->   g±√g²−ac
-    	2a				a
+		2a				a
 
 
 		
 */
 bool	Sphere::hit(const Ray& ray, float t_min, float t_max, HitRecord& rec) const
 {
-	Ray		l_ray;
+	Ray		l_ray = ray;
 	Vec3f	local_normal;
 	float	a;
 	float	b;
@@ -79,18 +79,22 @@ bool	Sphere::hit(const Ray& ray, float t_min, float t_max, HitRecord& rec) const
 	if (delta < 0.0f)
 		return (false);
 	//g±√g²−ac
-    //	2a
-	t = ((b - std::sqrt(delta)) / a);
+	//	2a
+	float	sqrtd = std::sqrt(delta);
+
+	t = (-b - sqrtd) / a;
+	
 	if (t < t_min || t > t_max)
 	{
-		t = ((b + std::sqrt(delta)) / a);
+		t = (-b + sqrtd) / a;
 		if (t < t_min || t > t_max)
 			return (false);
 	}
 	rec.t = t;
 	rec.material = _mat;
 	rec.point = ray(t);
-	rec.set_face_normal(ray, Vec3f::normalize(_transform * Vec3f::normalize(rec.point)));
+	Vec3f	world_normal = Vec3f::normalize(rec.point - _center);
+	rec.set_face_normal(ray, world_normal);
 	return (true);
 }
 
